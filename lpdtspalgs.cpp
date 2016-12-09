1ad045da6b8abc14a2981d78fd4a57d81de77a61
wendrey <lordwendrey@gmail.com>
2016-12-09 15:46:37 -0200

    Projeto 04

--- lpdtspalgs.cpp
+++ lpdtspalgs.cpp
@@ -173,7 +173,6 @@ try {
 
 	lowerBound = 1;
 	upperBound = sol.cost + 1;
-	cout << upperBound << endl;
 	GRBEnv env = GRBEnv();
 	GRBModel model = GRBModel(env);
 	model.set(GRB_StringAttr_ModelName, "LpdTsp");
@@ -305,13 +304,13 @@ try {
 		model.addConstr(expr == 1, "");
 	}		
 	
-	// (7): Ui - Uj + nXij <= n - 1, para i != j, 2 <= i,j <= n 
+/*	// (7): Ui - Uj + nXij <= n - 1, para i != j, 2 <= i,j <= n 
 	
 	for (ArcIt e(l.g); e != INVALID; ++e)
 		if (l.g.source(e) != l.depot && l.g.target(e) != l.depot)
 			model.addConstr(U[nodes[l.g.source(e)]] - U[nodes[l.g.target(e)]] + (l.n + 1) * X[nodes[l.g.source(e)]][nodes[l.g.target(e)]] <= l.n, "");
 	
-	// Objetivo: Minimizar C
+*/	// Objetivo: Minimizar C
 	
 	GRBLinExpr obj = C[l.n];
 	model.setObjective(obj, GRB_MINIMIZE);		
